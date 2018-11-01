2ea3a6003cff8d9f7705051ce9f8f956d9c7cd05
Erick-ECE <ericken15@ciencias.unam.mx>
2018-11-01 13:43:25 -0600

    fragment_fullImage

--- app/src/main/java/com/example/erick/demo/EventAdapter.kt
+++ app/src/main/java/com/example/erick/demo/EventAdapter.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.item_event.view.*
+import java.util.*
 
 class EventAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     private val eventList = mutableListOf<Event>()
@@ -23,8 +24,14 @@ class EventAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     override fun getItemCount(): Int = eventList.size
 
     override fun onBindViewHolder(itemView: RecyclerView.ViewHolder, p1: Int) {
+        val rightNow = Calendar.getInstance()
+        val Hours = rightNow.get(Calendar.HOUR) // return the hour in 24 hrs format (ranging from 0-23)
+
+        val minutes = if(rightNow.get(Calendar.MINUTE) < 10) "0"+ rightNow.get(Calendar.MINUTE).toString() else rightNow.get(Calendar.MINUTE).toString()
+
+        val meridian = if(rightNow.get(Calendar.AM_PM) == 0)  "AM" else "PM"
         with(itemView.itemView) {
-            txtTime.text = "2:92\nAM"
+            txtTime.text = Hours.toString() +":" + minutes +"\n" +  meridian
             //Get only the first sentence until a period is found.
             txtExplanation.text = eventList[p1].explanation!!.split(".")[0]
             txtTitle.text = eventList[p1].title
